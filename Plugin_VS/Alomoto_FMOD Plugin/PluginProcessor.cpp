485fe0b26282f342b0269be01cebbd64fef26f56
Wa1aby <elliotalomoto22@gmail.com>
2026-04-09 13:12:20 -0500

    Verb Started
    currently at delay unit stuff

--- Plugin_VS/Alomoto_FMOD Plugin/PluginProcessor.cpp
+++ Plugin_VS/Alomoto_FMOD Plugin/PluginProcessor.cpp
@@ -45,16 +45,51 @@ FMOD_RESULT SystemMix_Callback(FMOD_DSP_STATE* dsp_state, int stage);
 //    Parameters 
 //+++++++++++++++++++
 
+//Num_Parameters 
+enum
+{
+PARAM_INPUT_DIFFUSE_1 = 0, 
+PARAM_INPUT_DIFFUSE_2, 
+PARAM_DECAY_DIFFUSE_1, 
+PARAM_DECAY_DIFFUSE_2, 
+PARAM_BANDWIDTH, 
+PARAM_DECAY, 
+PARAM_DRY, 
+PARAM_WET, 
+PARAM_PRE_DELAY, 
+NUM_PARAMS
+};
+
+//Declarations 
+static FMOD_DSP_PARAMETER_DESC p_inputDiffuse1, p_inputDiffuse2, 
+p_decayDiffuse1, p_decayDiffuse2, p_bandwidth,
+p_decay, p_dry, p_wet, p_preDelay;
+
+//Pointer Array
+static FMOD_DSP_PARAMETER_DESC* PluginsParameters[NUM_PARAMS] =
+{
+&p_inputDiffuse1,
+&p_inputDiffuse2,
+&p_decayDiffuse1, 
+&p_decayDiffuse2,
+&p_bandwidth,
+&p_decay,
+&p_dry,
+&p_wet,
+&p_preDelay
+}; 
 
 // ++++++++++++++++++
 //    CallBacks
 //+++++++++++++++++++
 
+FMOD_RESULT Create_Callback(FMOD_DSP_STATE* dsp_state);
+FMOD_RESULT Release_Callback(FMOD_DSP_STATE* dsp_state);
 
-FMOD_DSP_DESCRIPTION PluginCallbacks =
+static FMOD_DSP_DESCRIPTION PluginCallbacks =
 {
     FMOD_PLUGIN_SDK_VERSION,    // version
-    "Kelly Hard Clip",          // name
+    "Advanced FMOD Reverb",          // name
     0x00010000,                 // plugin version
     1,                          // no. input buffers
     1,                          // no. output buffers
@@ -64,9 +99,8 @@ FMOD_DSP_DESCRIPTION PluginCallbacks =
     Read_Callback,              // read
     Process_Callback,           // process
     SetPosition_Callback,       // setposition
-    0,                          // no. parameter
-   NULL,                       // clears error since no parameters yet
-   // PluginsParameters,          // pointer to parameter descriptions
+    NUM_PARAMS,                 // no. parameter 
+    PluginsParameters,          // pointer to parameter descriptions
     SetFloat_Callback,          // Set float
     SetInt_Callback,            // Set int
     SetBool_Callback,           // Set bool
@@ -85,6 +119,64 @@ FMOD_DSP_DESCRIPTION PluginCallbacks =
 // ++++++++++++++++++
 //    Plugin Class (Processing) 
 //+++++++++++++++++++
+
+//Delay Buffer
+class DelayBuffer
+{
+public: 
+    DelayBuffer(int size) : m_buffer(size,0.0f){}
+    float& operator[](int index) { return m_buffer[index]; }
+    const float& operator[](int index)const { return m_buffer[index]; }
+
+private: 
+    std::vector<float>m_buffer; 
+};
+
+//Delay Unit
+class DelayUnit {
+public: 
+    DelayUnit();
+    ~DelayUnit(); 
+
+    //int. max delay in samples 
+
+    void Init(FMOD_DSP_STATE* dsp_state, int maxSamples);  //max delay length 
+
+    //create buffer based on channel count 
+
+    void CreateBuffers(int channels); //allocates mem. for channels 
+
+    //controls 
+    void SetDelayTime(float ms); //change how long delay is 
+    void SetFeedback(float feedback); 
+    float GetDelayTime()const; 
+
+    //core 
+    void WriteDelay(float value); //store sample into buffer
+    float GetDelaySample(); //read a delayed sample 
+    void TickChannel(); //write position forward
+
+private: 
+    DelayBuffer* m_delayBuffer; 
+    int m_writePos; 
+    float m_delayTime; 
+    float m_feedbackAmount; 
+    float m_sampleRate; 
+    int m_maxSamples; 
+    int m_numChannels; 
+};
+
+//Delay Unit Implementation 
+DelayUnit::DelayUnit()
+    : m_delayBuffer(nullptr)
+    , m_writePos(0)
+    , m_delayTime(10.0f)
+    , m_feedbackAmount(0.0f)
+    , m_sampleRate(44100)
+    , m_maxSamples(0)
+  //  , m_numOfChannels(-1)
+
+//Main Verb
 
 // ++++++++++++++++++
 //    Callback 2 (Dealing with Output) 
