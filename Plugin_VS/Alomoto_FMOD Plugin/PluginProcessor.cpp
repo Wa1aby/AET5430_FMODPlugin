7fc12fa391be3dc4c34b8119d191007361e22a76
Wa1aby <elliotalomoto22@gmail.com>
2026-04-14 11:18:23 -0500

    Main Plugin Processing

--- Plugin_VS/Alomoto_FMOD Plugin/PluginProcessor.cpp
+++ Plugin_VS/Alomoto_FMOD Plugin/PluginProcessor.cpp
@@ -83,8 +83,8 @@ static FMOD_DSP_PARAMETER_DESC* PluginsParameters[NUM_PARAMS] =
 //    CallBacks
 //+++++++++++++++++++
 
-FMOD_RESULT Create_Callback(FMOD_DSP_STATE* dsp_state);
-FMOD_RESULT Release_Callback(FMOD_DSP_STATE* dsp_state);
+//FMOD_RESULT Create_Callback(FMOD_DSP_STATE* dsp_state);
+//FMOD_RESULT Release_Callback(FMOD_DSP_STATE* dsp_state);
 
 static FMOD_DSP_DESCRIPTION PluginCallbacks =
 {
@@ -123,20 +123,20 @@ static FMOD_DSP_DESCRIPTION PluginCallbacks =
 //Delay Buffer
 class DelayBuffer
 {
-public: 
-    DelayBuffer(int size) : m_buffer(size,0.0f){}
+public:
+    DelayBuffer(int size) : m_buffer(size, 0.0f) {}
     float& operator[](int index) { return m_buffer[index]; }
     const float& operator[](int index)const { return m_buffer[index]; }
 
-private: 
-    std::vector<float>m_buffer; 
+private:
+    std::vector<float>m_buffer;
 };
 
 //Delay Unit
 class DelayUnit {
-public: 
+public:
     DelayUnit();
-    ~DelayUnit(); 
+    ~DelayUnit();
 
     //int. max delay in samples 
 
@@ -148,22 +148,22 @@ public:
 
     //controls 
     void SetDelayTime(float ms); //change how long delay is 
-    void SetFeedback(float feedback); 
-    float GetDelayTime()const; 
+    void SetFeedback(float feedback);
+    float GetDelayTime()const;
 
     //core 
     void WriteDelay(float value); //store sample into buffer
     float GetDelaySample(); //read a delayed sample 
     void TickChannel(); //write position forward
 
-private: 
-    DelayBuffer* m_delayBuffer; 
-    int m_writePos; 
-    float m_delayTime; 
-    float m_feedbackAmount; 
-    float m_sampleRate; 
-    int m_maxSamples; 
-    int m_numChannels; 
+private:
+    DelayBuffer* m_delayBuffer;
+    int m_writePos;
+    float m_delayTime;
+    float m_feedbackAmount;
+    int m_sampleRate;
+    int m_maxSamples; // changed from float to int 
+    int m_numChannels;
 };
 
 //Delay Unit Implementation 
@@ -172,12 +172,173 @@ DelayUnit::DelayUnit()
     , m_writePos(0)
     , m_delayTime(10.0f)
     , m_feedbackAmount(0.0f)
-    , m_sampleRate(44100)
+    , m_sampleRate(44100.0f)// I'm sure this will be fine? 
     , m_maxSamples(0)
-  //  , m_numOfChannels(-1)
+    , m_numChannels(-1)
+{
+}
+//Deconstructor (Mem Cleaner 9000) 
+DelayUnit::~DelayUnit()
+{
+    delete m_delayBuffer; 
+}
 
-//Main Verb
+// Init Bruv 
+void DelayUnit::Init(FMOD_DSP_STATE* dsp_state, int maxSamples)
+{
+    FMOD_RESULT result = FMOD_DSP_GETSAMPLERATE(dsp_state, &m_sampleRate); 
+    m_maxSamples = maxSamples; 
+    m_writePos = 0; 
+    m_numChannels = -1; 
+}
 
+void DelayUnit::CreateBuffers(int channels) 
+{
+    if (m_numChannels != channels && m_maxSamples > 0)
+    {
+        m_numChannels = channels; 
+        delete m_delayBuffer; 
+        m_delayBuffer = new DelayBuffer(m_maxSamples * channels);
+        m_writePos = 0;
+    }
+}
+
+//control medthods 
+void DelayUnit::SetDelayTime(float ms)
+{
+    m_delayTime = ms; 
+}
+
+void DelayUnit::SetFeedback(float feedback)
+{
+    m_feedbackAmount = feedback; 
+}
+
+float DelayUnit::GetDelayTime()const
+{
+    return m_delayTime; 
+}
+
+float DelayUnit::GetDelaySample()
+{
+    if (!m_delayBuffer || m_numChannels <= 0) return 0.0f;
+
+        int delaySamples = (int)(m_delayTime * 0.001f * m_sampleRate) * m_numChannels; 
+    int readPos = m_writePos - delaySamples; 
+
+    while (readPos < 0)readPos += (m_maxSamples * m_numChannels); 
+
+    return(*m_delayBuffer)[readPos]; 
+}
+
+void DelayUnit::TickChannel()
+{
+    if (!m_delayBuffer)return; 
+
+    m_writePos++; 
+    int bufferSize = m_maxSamples * m_numChannels; 
+    if (m_writePos >= bufferSize) m_writePos = 0; 
+}
+// ++++++++++++++++++
+//    Main Verb Pross (Dealing with Output) 
+//+++++++++++++++++++
+
+class Plugin
+{
+public:
+        Plugin(); 
+        ~Plugin(); 
+
+        void Init(FMOD_DSP_STATE* dsp_state); 
+        void Release(); 
+        void Process(float* inbuffer, float* outbugger, unsigned int length, int channels); 
+        void SetParameterFloat(int index, float value); 
+        void GetParameterFloat(int index, float* value); 
+
+private:  
+    DelayUnit* m_delay; 
+    float m_dry; 
+    float m_wet; 
+    float m_feedback; 
+    float m_delayTime; 
+};
+
+//Implementation  
+Plugin::Plugin(); 
+: m_delay(nullptr) 
+, m_dry(1.0f)
+, m_wet(0.5f)
+, m_feedback(0.3f)
+, m_delayTime(500.0f)
+, m_dry(1.0f)
+{
+}
+
+Plugin::~Plugin()
+{
+    delete m_delay; 
+}
+
+void Plugin::Init(FMOD_DSP_STATE* dsp_state)
+{
+    m_delay = newDelayUnit();
+    m_delay->Init(dsp_state, 44100);  // 1 second max delay
+}
+
+void Plugin::Release()
+{
+    delete m_delay
+        m_delay = nullprt; 
+}
+
+void Plugin::Process(float* inbuffer, float* outbuffer, unsigned int length, int channels)
+{
+    if (!m_delay)return; 
+
+    m_delay->SetdelayTime(m_delayTime); 
+    m_delay->SetFeedback(m_feedback); 
+    m_delay->CreateBuggers(channels); 
+   
+    for (unsigned int i = 0; i < length; i++)
+    {
+        for (int chh = 0; ch < channels; ch++)
+        {
+            float input = *inbuffer; 
+            float delayed = m_delay->GetDelaySample(); 
+            float output = (input * m_dry) + (delayed * m_wet); 
+                m_delay->WriteDelay(input + (delayed * m_feedback)); 
+                outbuffer = output;
+
+                inbuffer++; 
+                outbuffer++; 
+                m_delay->TickChannel(); 
+        }
+    }
+}
+
+void Plugin::SetParameterFloat(int index, float value)
+{
+    switch (index)
+    {
+    case PARAM_DRY: m_dry = value; break;
+    case PARAM_WET: m_wet = value; break;
+    case PARAM_DECAY: m_feedback = value; break;
+    case PARAM_DRY: m_delayTime = value; break;
+    default:break; 
+    }
+}
+
+void Plugin::GetParameterFloat(int index, float* value)
+{
+    switch (index)
+    {
+    case PARAM_DRY: m_dry = value; break;
+    case PARAM_WET: m_wet = value; break;
+    case PARAM_DECAY: m_feedback = value; break;
+    case PARAM_DRY: m_delayTime = value; break;
+    default:*value = 0.0f; break; 
+    }
+}
 // ++++++++++++++++++
 //    Callback 2 (Dealing with Output) 
 //+++++++++++++++++++
